73f378a4dec759714c243da6e8ee289cefc3a82a
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-04-04 20:07:12 -0500

    First commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = {
+ products: [
+    {
+     id:1,
+     name: 'Prada',
+     price: 110,
+     image: 'https://fimgs.net/mdimg/perfume/375x500.52383.jpg',
+   },
+   {
+    id:2,
+    name: 'Dior Homme',
+    price: 163,
+    image: 'https://fimgs.net/mdimg/perfume/375x500.58714.jpg',
+  },
+  {
+    id:3,
+    name: 'Terre d-Hermès',
+    price: 115,
+    image: 'https://fimgs.net/mdimg/perfume/375x500.17.jpg',
+  },
+
+    
+    ],
+};
+export default data;
